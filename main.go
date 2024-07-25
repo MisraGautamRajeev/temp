a2eb9dbe57e597a470844a55478b5c4053df38b9
MisraGautamRajeev <misra.gautam.rajeev.int@hotstar.com>
2024-07-25 17:06:06 +0530

    Triii

--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package temp
 
-import "github.com/rs/zerolog"
+import (
+	"github.com/rs/zerolog"
+	"math/rand"
+	"time"
+)
+
+var logLevels = []string{"debug", "info", "warn", "error", "fatal", "panic"}
+var logLevelMap = map[string]zerolog.Level{
+	"debug": zerolog.DebugLevel,
+	"info":  zerolog.InfoLevel,
+	"warn":  zerolog.WarnLevel,
+	"error": zerolog.ErrorLevel,
+	"fatal": zerolog.FatalLevel,
+	"panic": zerolog.PanicLevel,
+}
 
 func InitLogger() {
 	zerolog.SetGlobalLevel(zerolog.ErrorLevel)
+}
+
+func generateRandomLevel() {
+	rand.Seed(time.Now().UnixNano())
+
+	index := rand.Intn(len(logLevels))
+	newLogLevel := logLevelMap[logLevels[index]]
+	zerolog.SetGlobalLevel(newLogLevel)
 }
